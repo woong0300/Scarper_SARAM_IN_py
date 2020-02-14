48b2c1e0f5c633473e93084f3e46c350283f947a
woong0300 <woong0300@naver.com>
2020-02-14 17:15:05 +0900

    Python Web Scraper - Additional Step: getting Search word as input

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from saram_in import extract_saram_in_pages, extract_jobs
 from save import save_to_file
 
-SEARCH_WORD = "파이썬"
+SEARCH_WORD = input("검색어를 입력하세요:")
 PAGE_JOB_COUNT = 50
 SARAM_IN_URL = f"http://www.saramin.co.kr/zf_user/search/recruit?searchType=search&&searchword={SEARCH_WORD}&panel_type=&search_optional_item=y&search_done=y&panel_count=y&recruitSort=relation"
 
